6172ae0bfa53c5bcfcace7bf459d358b03d9b42a
Juncie <brandonmitchell5365@email.com>
2022-02-03 22:17:29 -0700

    Equipment and Damage UI Updated.

--- FrontEnd/components/Forms/Damage.js
+++ FrontEnd/components/Forms/Damage.js
@@ -1,19 +1,58 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import CustomButton from '../custom/CustomButton';
+import { View, Text, StyleSheet, Modal, TextInput, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
-const Damage = () => {
-	const [damages, setDamages] = useState({});
+import CustomButton from '../custom/CustomButton';
+import Container from '../custom/CustomContainer';
+
+const DamageReport = () => {
+	const navigation = useNavigation();
+	const [damage, setDamage] = useState({});
+	const [open, setOpen] = useState(false);
 
 	//get request to get damage
 
 	//if damage, push damage to state and show modal
 	//if no damage, show no damage modal
-
 	return (
-		<View>
+		<Container>
 			<Text style={textHeader}>Is there new damage to the vehicle?</Text>
-		</View>
+			<View style={formRow}>
+				<CustomButton text='Yes' onPress={() => setOpen(!open)} />
+
+				<CustomButton
+					text='No'
+					onPress={() => navigation.navigate('Daily Report', { screen: 'Vehicle' })}
+				/>
+			</View>
+			{open && ( //if damage, show modal with damage form
+				<Modal>
+					<View style={modalContainer}>
+						<Text style={modalTitle}>Damage</Text>
+						<Text style={modalSubtitle}>Please report the damage below.</Text>
+						<View style={formRow}>
+							<Text style={formLabel}>Area</Text>
+							<TextInput style={formInput} placeholder='Area' />
+						</View>
+						<View style={formRow}>
+							<Text style={formLabel}>Description</Text>
+							<TextInput style={formInput} placeholder='Description' />
+						</View>
+						<View style={formRow}>
+							<Text style={formLabel}>Attachments</Text>
+							<TextInput style={formInput} placeholder='Attachments' />
+						</View>
+					</View>
+					<Button
+						title='Submit'
+						onPress={() => {
+							setOpen(false);
+							navigation.navigate('Daily Report', { screen: 'Vehicle' });
+						}}
+					/>
+				</Modal>
+			)}
+		</Container>
 	);
 };
 
@@ -21,10 +60,25 @@ const styles = StyleSheet.create({
 	textHeader: {
 		fontSize: 20,
 		fontWeight: 'bold',
-		marginBottom: 10,
+		marginVertical: 10,
+		alignSelf: 'center',
+	},
+	modalContainer: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#fff',
 	},
 });
 
-const { textHeader } = styles;
+const {
+	textHeader,
+	formRow,
+	formLabel,
+	formInput,
+	modalContainer,
+	modalTitle,
+	modalSubtitle,
+} = styles;
 
-export default Damage;
+export default DamageReport;
--- FrontEnd/components/Forms/Equipment.js
+++ FrontEnd/components/Forms/Equipment.js
@@ -1,105 +1,165 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Button, Modal, Switch } from 'react-native';
+import {
+	View,
+	Text,
+	StyleSheet,
+	Button,
+	Modal,
+	Switch,
+	TextInput,
+} from 'react-native';
 import CustomButton from '../custom/CustomButton';
 
 import Container from '../custom/CustomContainer';
-import {useNavigation} from '@react-navigation/native';
+import Input from '../custom/CustomInput';
+import { useNavigation } from '@react-navigation/native';
 
 export default Equipment = () => {
 	const [equipment, setEquipment] = useState({});
-
-	const [phone, setPhone] = useState(false);
 	const [showPhone, setShowPhone] = useState(false);
-
-	const [radio, setRadio] = useState(false);
 	const [showRadio, setShowRadio] = useState(false);
-
-	const [keys, setKeys] = useState(false);
 	const [showKeys, setShowKeys] = useState(false);
-
-	const [flashlight, setFlashlight] = useState(false);
 	const [showFlashlight, setShowFlashlight] = useState(false);
-
-	const [other, setOther] = useState(false);
 	const [showOther, setShowOther] = useState(false);
+	console.log(equipment);
 
-	const WittmannEquipment = [
-		{
-			name: 'Phone',
-			value: phone,
-			show: showPhone,
-			set: setShowPhone,
-			setValue: setPhone,
-		},
-		{
-			name: 'Radio',
-			value: radio,
-			show: showRadio,
-			set: setShowRadio,
-			setValue: setRadio,
-		},
-		{
-			name: 'Keys',
-			value: keys,
-			show: showKeys,
-			set: setShowKeys,
-			setValue: setKeys,
-		},
-		{
-			name: 'Flashlight',
-			value: flashlight,
-			show: showFlashlight,
-			set: setShowFlashlight,
-			setValue: setFlashlight,
-		},
-		{
-			name: 'Other',
-			value: other,
-			show: showOther,
-			set: setShowOther,
-			setValue: setOther,
-		},
-	];
+	const navigation = useNavigation();
 
-	//map through the array of objects and return the name and value
-	const WittmannEquipmentList = WittmannEquipment.map((item, index) => {
-		return (
-			<View key={index}>
-				<Text>{item.name}</Text>
+	return (
+		<Container style={formContainer}>
+			<Text style={tabTitle}>Equipment</Text>
+			<Text style={tabSubtitle}>
+				Please enter any equipment you received for your shift.
+			</Text>
+
+			<View style={formRow}>
+				<Text style={formLabel}>Phone</Text>
 				<Switch
-					value={item.value}
 					trackColor={{ false: '#767577', true: '#81b0ff' }}
-					onValueChange={() => {
-						item.set(!item.show);
-					}}
+					thumbColor={showPhone ? '#f5dd4b' : '#f4f3f4'}
+					ios_backgroundColor='#3e3e3e'
+					onValueChange={() => setShowPhone(!showPhone)}
+					value={showPhone}
 				/>
+				{showPhone && (
+					<View style={formRow}>
+						<Input
+							style={formInput}
+							placeholder='Phone Number'
+							onChangeText={text => setEquipment({ ...equipment, phone: text })}
+						/>
+					</View>
+				)}
 			</View>
-		);
-	});
 
-	return <Container style={formContainer}>
-		{WittmannEquipmentList}
-		<CustomButton title="Next" onPress={} />
-	</Container>;
+			<View style={formRow}>
+				<Text style={formLabel}>Radio</Text>
+				<Switch
+					trackColor={{ false: '#767577', true: '#81b0ff' }}
+					thumbColor={showRadio ? '#f5dd4b' : '#f4f3f4'}
+					ios_backgroundColor='#3e3e3e'
+					onValueChange={() => setShowRadio(!showRadio)}
+					value={showRadio}
+				/>
+				{showRadio && ( // Radio is selected and should be shown in the form below
+					<View style={formRow}>
+						<Input
+							style={formInput}
+							placeholder='Radio Number'
+							onChangeText={text => setEquipment({ ...equipment, radio: text })}
+						/>
+					</View>
+				)}
+			</View>
+
+			<View style={formRow}>
+				<Text style={formLabel}>Keys</Text>
+				<Switch
+					trackColor={{ false: '#767577', true: '#81b0ff' }}
+					thumbColor={showKeys ? '#f5dd4b' : '#f4f3f4'}
+					ios_backgroundColor='#3e3e3e'
+					onValueChange={() => setShowKeys(!showKeys)}
+					value={showKeys}
+				/>
+				{showKeys && ( // Keys is selected and should be shown in the form below
+					<View style={formRow}>
+						<Input
+							style={formInput}
+							placeholder='Key Number'
+							onChangeText={text => setEquipment({ ...equipment, keys: text })}
+						/>
+					</View>
+				)}
+			</View>
+
+			<View style={formRow}>
+				<Text style={formLabel}>Flashlight</Text>
+				<Switch
+					trackColor={{ false: '#767577', true: '#81b0ff' }}
+					thumbColor={showFlashlight ? '#f5dd4b' : '#f4f3f4'}
+					ios_backgroundColor='#3e3e3e'
+					onValueChange={() => setShowFlashlight(!showFlashlight)}
+					value={showFlashlight}
+				/>
+				{showFlashlight && ( // Flashlight is selected and should be shown in the form below
+					<View>
+						<TextInput
+							style={formInput}
+							placeholder='Flashlight'
+							onChangeText={text => setEquipment({ ...equipment, flashlight: text })}
+						/>
+					</View>
+				)}
+			</View>
+
+			<View style={formRow}>
+				<Text style={formLabel}>Other</Text>
+				<Switch
+					trackColor={{ false: '#767577', true: '#81b0ff' }}
+					thumbColor={showOther ? '#f5dd4b' : '#f4f3f4'}
+					ios_backgroundColor='#3e3e3e'
+					onValueChange={() => setShowOther(!showOther)}
+					value={showOther}
+				/>
+				{showOther && (
+					<View style={formRow}>
+						<TextInput
+							style={formInput}
+							placeholder='Other'
+							onChangeText={text => setEquipment({ ...equipment, other: text })}
+						/>
+					</View>
+				)}
+			</View>
+			<CustomButton
+				text='Next'
+				onPress={() => navigation.navigate('Daily Report', { screen: 'Damage' })}
+			/>
+		</Container>
+	);
 };
 
 const styles = StyleSheet.create({
+	formContainer: {
+		marginTop: 20,
+		marginHorizontal: 20,
+	},
 	tabTitle: {
 		fontSize: 20,
 		fontWeight: 'bold',
 		alignSelf: 'center',
 	},
-	formContainer: {
-		flex: 1,
-		flexDirection: 'column',
-		justifyContent: 'space-evenly',
+	tabSubtitle: {
+		fontSize: 15,
+		marginVertical: 10,
+		alignSelf: 'center',
+		textAlign: 'center',
 	},
-	options: {
+	formRow: {
 		flexDirection: 'row',
-		justifyContent: 'space-evenly',
-		alignItems: 'center',
-		marginBottom: 10,
+		justifyContent: 'space-between',
 	},
 });
 
-const { tabTitle, formContainer, options } = styles;
+const { tabTitle, formContainer, tabSubtitle, formRow, formLabel, formInput } =
+	styles;
